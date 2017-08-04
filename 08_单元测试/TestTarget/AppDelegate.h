4691cee3bf290ea7d2edcbf579a566e36c9377d6
zhouheng <fm939071955@163.com>
2017-08-04 15:56:42 +0800

    多个Target的添加
    利用多个Target，在一个工程中管理多个不同版本或内容大同小异的项目

---
+++ 08_单元测试/TestTarget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTarget
+//
+//  Created by Windy on 2017/8/2.
+//  Copyright © 2017年 Windy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 08_单元测试/TestTarget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTarget
+//
+//  Created by Windy on 2017/8/2.
+//  Copyright © 2017年 Windy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
