01d552a36d1754a9652c7bc844d4f8de9946bb6d
zhou heng <fm939071955@163.com>
2018-08-27 09:53:13 +0800

    interviewQ
    注释添加

--- 15_interviewQ/15_interviewQ/SuperClassTest/Dog.m
+++ 15_interviewQ/15_interviewQ/SuperClassTest/Dog.m
@@ -12,10 +12,11 @@
 
 - (instancetype)init {
     if (self = [super init]) {
-        NSLog(@"111 --- %@", NSStringFromClass([self class]));
-        NSLog(@"222 --- %@", NSStringFromClass([super class]));
-        NSLog(@"333 --- %@", NSStringFromClass([self superclass]));
-        NSLog(@"444 --- %@", NSStringFromClass([super superclass]));
+        NSLog(@"111 --- %@", NSStringFromClass([self class]));// Dog
+        //只是通知父类去调用class方法，子类重写了这个方法，所以还是调用本类的class，所以还是Dog
+        NSLog(@"222 --- %@", NSStringFromClass([super class]));// Dog
+        NSLog(@"333 --- %@", NSStringFromClass([self superclass]));// Person
+        NSLog(@"444 --- %@", NSStringFromClass([super superclass]));// Person
     }
     return self;
 }
